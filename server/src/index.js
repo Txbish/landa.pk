f239e0e34d55388ddb81a49df5a8332b859a9163
Tabish <114801188+Txbish@users.noreply.github.com>
2025-04-19 13:40:23 +0500

    added a /

--- server/src/index.js
+++ server/src/index.js
@@ -64,7 +64,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/seller-requests", sellerRequestRoutes);
-app.use("api/cart", cartRoutes);
+app.use("/api/cart", cartRoutes);
 // === Default Route ===
 app.get("/", (req, res) => {
   res.send("Welcome to Landa.pk API!");
